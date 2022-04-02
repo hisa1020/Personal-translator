e0adb3c10e7da107da7cc58e1c18b28090c0f7cc
hisa1020 <hisa1020gueen@gmail.com>
2022-04-02 13:40:35 +0900

    feat: deviseの導入 personaltranslatorの削除

--- app/controllers/personaltranslator/top_controller.rb
+++
@@ -1,4 +0,0 @@
-class Personaltranslator::TopController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
+  root 'top#index'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  namespace :personaltranslator do
-    get '/', to: 'top#index'
-    get 'index', to: 'top#index'
-  end
 end
