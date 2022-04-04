c8cc45f93402e0456a481603ff0b2e8ffb23c52d
hisa1020 <hisa1020gueen@gmail.com>
2022-04-04 18:30:13 +0900

    feat: user/mypageの実装、user_controlllerの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
+  get 'users/mypage', to: 'users#show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
