4f9d33773effb10d4016a2baedaa66dacb8498e3
hisa1020 <hisa1020gueen@gmail.com>
2022-04-17 13:13:46 +0900

    feat: question/index/show/new/edit/delete/create/updateを追加,レイアウトの追加変更

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@ class Question < ApplicationRecord
   validates :q_title, presence: true
   validates :q_content, presence: true
   belongs_to :user
+
+  def user
+    User.find_by(id: user_id)
+  end
 end
