2dc7fa49a9a69011fa824c7ab8339a6d0a66b1d3
hisa1020 <hisa1020gueen@gmail.com>
2022-05-27 23:07:07 +0900

    update for heroku error of rate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_041236) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_27_134812) do
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "user_id", null: false
